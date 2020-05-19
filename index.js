2a97526913e4211c0dc889312be692b79786bfd2
tugba <ttugbasen@gmail.com>
2020-05-19 20:08:45 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code here
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food
+        this.drink = drink
+    }
+}
+
+//let newBreakfast = new Breakfast(peynir, cay)
+
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.salad = salad
+        this.soup = soup
+        this.drink = drink
+    }   
+}
+
+//let newLunch = new Lunch(coban, sehriye, kola)
+
+class Dinner {
+    constructor(salad, soup, entree, dessert) {
+        this.salad = salad
+        this.soup = soup
+        this.entree = entree
+        this._dessert = dessert
+    }
+}
+
